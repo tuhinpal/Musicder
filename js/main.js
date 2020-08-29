1c838c4ccbad1901245d240426c9d9314ccd66b1
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-08-29 16:16:52 +0530

    Updated API Url

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ function firstF() {
     xhr.send();
 };
 
-getJSON('https://api.musicder.workers.dev/result/?query='+ resj,  function(err, data) {
+getJSON('https://api.musicder.tk/result/?query='+ resj,  function(err, data) {
     
     if (err != null) {
         console.error(err);
@@ -52,54 +52,54 @@ getJSON('https://api.musicder.workers.dev/result/?query='+ resj,  function(err, 
        document.getElementById("footer").style.display = "block";    
 	   
         var songnameone = `${data[0].song}`
-		var albumone = `${data[0].album}`
+	var albumone = `${data[0].album}`
         var imageone = `${data[0].image}`
         var linkone = `${data[0].media_url}`
         var artistone = `${data[0].singers}`
 		
         var songnametwo = `${data[1].song}`
-		var albumtwo = `${data[1].album}`
+	var albumtwo = `${data[1].album}`
         var imagetwo = `${data[1].image}`
         var linktwo = `${data[1].media_url}`
         var artisttwo = `${data[1].singers}`
 		
         var songnamethree = `${data[2].song}`
-		var albumthree = `${data[2].album}`
+	var albumthree = `${data[2].album}`
         var imagethree = `${data[2].image}`
         var linkthree = `${data[2].media_url}`
         var artistthree = `${data[2].singers}`	
 
         var songnamefour = `${data[3].song}`
-		var albumfour = `${data[3].album}`
+	var albumfour = `${data[3].album}`
         var imagefour = `${data[3].image}`
         var linkfour = `${data[3].media_url}`
         var artistfour = `${data[3].singers}`	
 
         var songnamefive = `${data[4].song}`
-		var albumfive = `${data[4].album}`
+	var albumfive = `${data[4].album}`
         var imagefive = `${data[4].image}`
         var linkfive = `${data[4].media_url}`
         var artistfive = `${data[4].singers}`			
 		
         document.getElementById("tu").innerHTML ="<h1 class='headingtwo'>"+songnameone+"</h1><br><p class='parag'>"+artistone+"</p><br><p class='parag'> Album : "+albumone+"</p>"
-		document.getElementById("imageone").src = imageone
-		document.getElementById("dldone").action = linkone
+	document.getElementById("imageone").src = imageone
+	document.getElementById("dldone").action = linkone
 		
         document.getElementById("hin").innerHTML ="<h1 class='headingtwo'>"+songnametwo+"</h1><br><p class='parag'>"+artisttwo+"</p><br><p class='parag'> Album : "+albumtwo+"</p>"
-		document.getElementById("imagetwo").src = imagetwo
-		document.getElementById("dldtwo").action = linktwo	
+	document.getElementById("imagetwo").src = imagetwo
+	document.getElementById("dldtwo").action = linktwo	
 		
         document.getElementById("life").innerHTML ="<h1 class='headingtwo'>"+songnamethree+"</h1><br><p class='parag'>"+artistthree+"</p><br><p class='parag'> Album : "+albumthree+"</p>"
-		document.getElementById("imagethree").src = imagethree
-		document.getElementById("dldthree").action = linkthree
+	document.getElementById("imagethree").src = imagethree
+	document.getElementById("dldthree").action = linkthree
 		
         document.getElementById("iamtuhin").innerHTML ="<h1 class='headingtwo'>"+songnamefour+"</h1><br><p class='parag'>"+artistfour+"</p><br><p class='parag'> Album : "+albumfour+"</p>"
-		document.getElementById("imagefour").src = imagefour
-		document.getElementById("dldfour").action = linkfour
+	document.getElementById("imagefour").src = imagefour
+	document.getElementById("dldfour").action = linkfour
 		
         document.getElementById("ga").innerHTML ="<h1 class='headingtwo'>"+songnamefive+"</h1><br><p class='parag'>"+artistfive+"</p><br><p class='parag'> Album : "+albumfive+"</p>"
-		document.getElementById("imagefive").src = imagefive
-		document.getElementById("dldfive").action = linkfive
+	document.getElementById("imagefive").src = imagefive
+	document.getElementById("dldfive").action = linkfive
     }
 });
 }
