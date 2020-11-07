4e2f9958accdce21c28646b336dbce93c483ecaa
cachecleanerjeet <me@thetuhin.com>
2020-11-07 16:42:25 +0530

    Code Quality Improved

--- js/main.js
+++ js/main.js
@@ -4,124 +4,77 @@
  * Visit https://tu.hin.life
  */
 
-function firstF() {
-    var songname = document.getElementById("search").value;
-    var snformat = songname.replace(/ /g, "+");
+function searchF() {
+    var songquery = document.getElementById("search").value.replace(/ /g, "+");
     document.getElementById("firsts").style.display = "none";
     document.getElementById("footer").style.display = "block";
     document.getElementById("loads").style.display = "block";
-    var getJSON = function(url, callback) {
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-
-        xhr.onload = function() {
-
-            var status = xhr.status;
-
-            if (status == 200) {
-                callback(null, xhr.response);
-            } else {
-                callback(status);
-            }
-        };
-
-        xhr.send();
-    };
-
-    getJSON('https://songapi.thetuhin.com/search?query=' + snformat, function(err, data) {
-
-        if (err != null) {
-            console.error(err);
-        } else {
+    var search = new XMLHttpRequest();
+    search.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
             document.getElementById("loads").style.display = "none";
             document.getElementById("seconds").style.display = "block";
 
-            var tempone = `${data[0]}`;
-            var temptwo = `${data[1]}`;
-            var tempthree = `${data[2]}`;
-            var tempfour = `${data[3]}`;
-            var tempfive = `${data[4]}`;
+            var data = JSON.parse(this.responseText);
 
-
-            if (tempone === "undefined") {
+            if (data[0] == undefined) {
                 document.getElementById("errresult").style.display = "block";
                 document.getElementById("sfirst").style.display = "none";
             } else {
                 document.getElementById("errresult").style.display = "none";
-                var songnameone = `${data[0].title}`;
-                var albumone = `${data[0].album}`;
-                var imageone = `${data[0].image}`;
-                var artistone = `${data[0].more_info.singers}`;
-                var id_one = `${data[0].id}`;
-                document.getElementById("tu").innerHTML = "<h1 class='headingtwo'>" + songnameone + "</h1><br><p class='parag'>" + artistone + "</p><br><p class='parag'> Album : " + albumone + "</p>";
-                document.getElementById("imageone").src = imageone;
-                document.getElementById("dldone").href = "download/?id=" + id_one;
-                document.getElementById("playone").href = "play/?id=" + id_one + "&n=" + songnameone;
+                document.getElementById("sonec").innerHTML = `<h1 class="headingtwo">${data[0].title}</h1><br><p class="parag">${data[0].more_info.singers}<br><br>Album : ${data[0].album}</p>`;
+                document.getElementById("imageone").src = data[0].image;
+                document.getElementById("dldone").href = `download/?id=${data[0].id}`;
+                document.getElementById("playone").href = `play/?id=${data[0].id}&n=${data[0].title}`;
             }
 
 
-            if (temptwo === "undefined") {
+            if (data[1] == undefined) {
                 document.getElementById("ssecond").style.display = "none";
             } else {
-                var songnametwo = `${data[1].title}`;
-                var albumtwo = `${data[1].album}`;
-                var imagetwo = `${data[1].image}`;
-                var artisttwo = `${data[1].more_info.singers}`;
-                var id_two = `${data[1].id}`;
-                document.getElementById("hin").innerHTML = "<h1 class='headingtwo'>" + songnametwo + "</h1><br><p class='parag'>" + artisttwo + "</p><br><p class='parag'> Album : " + albumtwo + "</p>";
-                document.getElementById("imagetwo").src = imagetwo;
-                document.getElementById("dldtwo").href = "download/?id=" + id_two;
-                document.getElementById("playtwo").href = "play/?id=" + id_two + "&n=" + songnametwo;
+                document.getElementById("stwoc").innerHTML = `<h1 class="headingtwo">${data[1].title}</h1><br><p class="parag">${data[1].more_info.singers}<br><br>Album : ${data[1].album}</p>`;
+                document.getElementById("imagetwo").src = data[1].image;
+                document.getElementById("dldtwo").href = `download/?id=${data[1].id}`;
+                document.getElementById("playtwo").href = `play/?id=${data[1].id}&n=${data[1].title}`;
             }
 
 
-            if (tempthree === "undefined") {
+            if (data[2] == undefined) {
                 document.getElementById("sthird").style.display = "none";
             } else {
-                var songnamethree = `${data[2].title}`;
-                var albumthree = `${data[2].album}`;
-                var imagethree = `${data[2].image}`;
-                var artistthree = `${data[2].more_info.singers}`;
-                var id_three = `${data[2].id}`;
-                document.getElementById("life").innerHTML = "<h1 class='headingtwo'>" + songnamethree + "</h1><br><p class='parag'>" + artistthree + "</p><br><p class='parag'> Album : " + albumthree + "</p>";
-                document.getElementById("imagethree").src = imagethree;
-                document.getElementById("dldthree").href = "download/?id=" + id_three;
-                document.getElementById("playthree").href = "play/?id=" + id_three + "&n=" + songnamethree;
+                document.getElementById("sthreec").innerHTML = `<h1 class="headingtwo">${data[2].title}</h1><br><p class="parag">${data[2].more_info.singers}<br><br>Album : ${data[2].album}</p>`;
+                document.getElementById("imagethree").src = data[2].image;
+                document.getElementById("dldthree").href = `download/?id=${data[2].id}`;
+                document.getElementById("playthree").href = `play/?id=${data[2].id}&n=${data[2].title}`;
             }
 
 
-            if (tempfour === "undefined") {
+            if (data[3] == undefined) {
                 document.getElementById("sfourth").style.display = "none";
             } else {
-                var songnamefour = `${data[3].title}`;
-                var albumfour = `${data[3].album}`;
-                var imagefour = `${data[3].image}`;
-                var artistfour = `${data[3].more_info.singers}`;
-                var id_four = `${data[3].id}`;
-                document.getElementById("iamtuhin").innerHTML = "<h1 class='headingtwo'>" + songnamefour + "</h1><br><p class='parag'>" + artistfour + "</p><br><p class='parag'> Album : " + albumfour + "</p>";
-                document.getElementById("imagefour").src = imagefour;
-                document.getElementById("dldfour").href = "download/?id=" + id_four;
-                document.getElementById("playfour").href = "play/?id=" + id_four + "&n=" + songnamefour;
+                document.getElementById("sfourc").innerHTML = `<h1 class="headingtwo">${data[3].title}</h1><br><p class="parag">${data[3].more_info.singers}<br><br>Album : ${data[3].album}</p>`;
+                document.getElementById("imagefour").src = data[3].image;
+                document.getElementById("dldfour").href = `download/?id=${data[3].id}`;
+                document.getElementById("playfour").href = `play/?id=${data[3].id}&n=${data[3].title}`;
             }
 
-            if (tempfive === "undefined") {
+            if (data[4] == undefined) {
                 document.getElementById("sfifth").style.display = "none";
             } else {
-                var songnamefive = `${data[4].title}`;
-                var albumfive = `${data[4].album}`;
-                var imagefive = `${data[4].image}`;
-                var artistfive = `${data[4].more_info.singers}`;
-                var id_five = `${data[4].id}`;
-                document.getElementById("ga").innerHTML = "<h1 class='headingtwo'>" + songnamefive + "</h1><br><p class='parag'>" + artistfive + "</p><br><p class='parag'> Album : " + albumfive + "</p>";
-                document.getElementById("imagefive").src = imagefive;
-                document.getElementById("dldfive").href = "download/?id=" + id_five;
-                document.getElementById("playfive").href = "play/?id=" + id_five + "&n=" + songnamefive;
+                document.getElementById("sfivec").innerHTML = `<h1 class="headingtwo">${data[4].title}</h1><br><p class="parag">${data[4].more_info.singers}<br><br>Album : ${data[4].album}</p>`;
+                document.getElementById("imagefive").src = data[4].image;
+                document.getElementById("dldfive").href = `download/?id=${data[4].id}`;
+                document.getElementById("playfive").href = `play/?id=${data[4].id}&n=${data[4].title}`;
             }
         }
     });
+
+    search.open("GET", "https://songapi.thetuhin.com/search?query=" + songquery);
+    search.send();
 }
+
+
 window.addEventListener('keydown', function(e) {
     if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
         if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
